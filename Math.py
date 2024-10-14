f95c78639c3b6721e2311f4d367f8c35f5892a68
Abdul Barik <abdulbarik1997m@gmail.com>
2024-10-15 01:25:24 +0600

    Math

---
+++ Math.py
@@ -0,0 +1,21 @@
+# Math
+# Built-in Math Functions
+x = min(15, 128, 215)
+y = max(55, 120, 215)
+
+print(x)
+print(y)
+
+# The abs() function returns the absolute (positive) value of the specified number:
+x = abs(-15.55)
+print(x)
+
+x = pow(5, 3)
+print(x)
+
+# The Math Module
+import math
+print(math.sqrt(16))
+print(math.ceil(16.52))
+print(math.floor(16.49))
+print(math.pi)
