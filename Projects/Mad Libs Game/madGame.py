7795b01fbd7335bc09c6023ccab5a8dce270aa18
Abdul Barik <abdulbarik1997m@gmail.com>
2024-10-17 01:01:42 +0600

    Mad Libs Game

---
+++ Projects/Mad Libs Game/madGame.py
@@ -0,0 +1,7 @@
+userName = input("What is Your Name:")
+fatherName = input("What is Your Father Name:")
+motherName = input("What is Your Mother Name:")
+study = input("Which Class Do You Read in:")
+
+print(f"My Doggy Name is {userName}, My Doggy Father Name is {fatherName}, My Doggy Mother Name is {motherName}, My Doggy Name is Doggy Study in Class {study}")
+
